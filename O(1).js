4d1c538f55f66bbfb62af98c5b8c26a0e8159c4a
charan <charan9487@gmail.com>
2020-09-08 09:15:31 +0530

    add excercise file

---
+++ O(1).js
@@ -0,0 +1,9 @@
+//^O(1) ->constant time
+//*Excellent
+//^Takes specific element and operation is done on it
+const boxes = [0, 1, 2, 3, 4, 5];
+function logFirstTwoBoxes(boxes) {
+            console.log(boxes[0]);//*O(1)
+            console.log(boxes[1]);//*O(1)
+}
+logFirstTwoBoxes(boxes);//*0(2)
